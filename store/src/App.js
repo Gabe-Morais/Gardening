4c48b4e69aedc2e2dddf78ec70b8929b1f67b8ab
Gabriel Morais <gabe-morais@outlook.com>
2023-12-09 21:37:17 -0300

    Router e Banner

--- store/src/App.js
+++ store/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
---
+++ store/src/components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="text-div">
+        <h1 className="welcome-title">Bem-vindo!</h1>
+        <p className="welcome-text">
+          Descubra a diferença que a excelência faz na jardinagem! Navegue por
+          nossa seleção de adubos de alta qualidade, ferramentas especializadas
+          e acessórios essenciais. Cultive um jardim exuberante e saudável com
+          os melhores produtos do mercado.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
