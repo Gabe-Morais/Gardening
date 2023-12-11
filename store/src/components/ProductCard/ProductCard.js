c3f83caf0e477a82faab1cf4870f41f88ec936a7
Gabriel Morais <gabe-morais@outlook.com>
2023-12-11 13:48:34 -0300

    Update ProductCard.js

--- store/src/components/ProductCard/ProductCard.js
+++ store/src/components/ProductCard/ProductCard.js
@@ -29,13 +29,13 @@ const ProductCard = ({ product }) => {
           <path
             fill="#408080"
             stroke="#408080"
-            stroke-linejoin="round"
+            strokeLinejoin="round"
             d="M24.62 7.923c0 1.827-.701 3.58-1.954 4.879-2.882 2.988-5.678 6.104-8.669 8.983a1.728 1.728 0 0 1-2.428-.053l-8.616-8.93c-2.604-2.7-2.604-7.058 0-9.757a6.59 6.59 0 0 1 9.543 0l.314.325.313-.325A6.628 6.628 0 0 1 17.895 1c1.793 0 3.51.737 4.771 2.045a7.027 7.027 0 0 1 1.954 4.878Z"
           />
         ) : (
           <path
             stroke="#143939"
-            stroke-linejoin="round"
+            strokeLinejoin="round"
             d="M24.62 7.923c0 1.827-.701 3.58-1.954 4.879-2.882 2.988-5.678 6.104-8.669 8.983a1.728 1.728 0 0 1-2.428-.053l-8.616-8.93c-2.604-2.7-2.604-7.058 0-9.757a6.59 6.59 0 0 1 9.543 0l.314.325.313-.325A6.628 6.628 0 0 1 17.895 1c1.793 0 3.51.737 4.771 2.045a7.027 7.027 0 0 1 1.954 4.878Z"
           />
         )}
@@ -68,19 +68,19 @@ const ProductCard = ({ product }) => {
           <g>
             <path
               stroke="#5B5B5B"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="3"
               d="M3.545 7.15h39.142l-6.18 20.601H9.725l-6.18-20.6Zm0 0L2 2m18.026 32.961a3.09 3.09 0 0 1-6.18 0m18.54 0a3.09 3.09 0 0 1-6.18 0"
             />
-            <path stroke="#408080" stroke-width="4" d="M15 11.724 22 20 43 4" />
+            <path stroke="#408080" strokeWidth="4" d="M15 11.724 22 20 43 4" />
           </g>
         ) : (
           <path
             stroke="#5B5B5B"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="3"
             d="M3.545 7.15h39.142l-6.18 20.601H9.725l-6.18-20.6Zm0 0L2 2m15.95 15.45h4.12m0 0h4.12m-4.12 0v-4.12m0 4.12v4.12m-2.044 13.391a3.09 3.09 0 0 1-6.18 0m18.54 0a3.09 3.09 0 0 1-6.18 0"
           />
         )}
